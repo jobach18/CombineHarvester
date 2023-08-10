ab916bb7d966df6dc57c17ec0ed86bf41d60be2b
Joern Bach <bachjoer@naf-cms21.desy.de>
2023-08-10 16:39:53 +0200

    new plots for validation

--- ahtt/visualization/pd_vis.py
+++ ahtt/visualization/pd_vis.py
@@ -13,13 +13,14 @@ def main():
     #load the data from a specified directory with pandas dataframes in h5 files
     file_path = "../data/" + args.Tag+"/train_data_"+ args.Expectation  + "_full.h5"
     with pd.HDFStore(file_path, mode='r') as store:
-        print(store)
         if 'df' in store:
             df = store['df']
+            #df.convert_dtypes(convert_floating=True)
+            df = df.apply(pd.to_numeric, errors='coerce')
     print(f'the dataset has {len(df)} entries')
     fig, (ax1) = plt.subplots(1,1, figsize=(10,10))
     pd.plotting.scatter_matrix(df, ax=ax1)
-    fig.savefig('plots/scattermatrix_'+args.Tag+'_'+args.Expectation+'.pdf', dpi=100)
+    fig.savefig('plots/scattermatrix_'+args.Tag+'_'+args.Expectation+'.pdf')
 
 if __name__=="__main__":
     main()
